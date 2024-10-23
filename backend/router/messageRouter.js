c5cef166f8290f88aa58356d0ab5d2961466fb9f
Anuththa <157110151+Anuththa@users.noreply.github.com>
2024-10-23 16:19:14 +0530

    messageRouter_update

--- backend/router/messageRouter.js
+++ backend/router/messageRouter.js
@@ -7,14 +7,3 @@ router.post("/send", sendMessage);
 router.get("/getall", isAdminAuthenticated, getAllMessages);
 
 export default router;
-
-/*import express from "express";
-import {getAllMessages, sendMessage} from "../controller/messageController.js";
-import { isAuthenticated } from "../middlewares/auth.js";
-const router = express.Router();
-
-router.post("/send", sendMessage);
-router.get("/getall", isAuthenticated, getAllMessages);
-
-export default router;
-*/
